be3a34bf74ee6f6f36d3b9f8eda69e8c354280a3
ring <2522564501@qq.com>
2021-11-04 16:10:11 +0800

    编辑了header/banner/footer

---
+++ src/components/top.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="top">
+        <p class="main">育路出国留学网-专注出国留学提供高质量的留学规划服务</p>
+        <div class="header">
+            <div class="left">
+                <div class="logo"><img src="../assets/yuloo-logo.png" alt=""></div>
+                <div class="text">
+                    <h2>出国留学</h2>
+                </div>
+            </div>
+            <div class="right">
+                <button>☰</button>
+            </div>
+        </div>
+        <div class="banner">
+            <img src="../assets/banner2.jpg" alt="">
+        </div>
+        <p class="main">当前位置：首页>出国留学网</p>
+    </div>
+</template>
+<style lang="less">
+.top{
+    width: 100%;
+    .main{
+        padding: 0.1rem;
+        box-sizing: border-box;
+        width: 100%;
+        font-size: .13rem;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        color: #838c94;
+    }
+    .header{
+        width: 100%;
+        // height: .5rem;
+        background: #fff;
+        padding: 0.1rem;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-between;
+        .left{
+            width: 70%;
+            display: flex;
+            justify-content: space-between;
+            .logo{
+                width: 50%;
+                img{
+                    width: 100%;
+                }
+            }
+            .text{
+                width: 50%;
+                h2{
+                    font-size: .15rem;
+                    padding: .1rem;
+                    width: 60%;
+                    text-align: center;
+                    border-radius: .1rem;
+                    color: #fff;
+                    background:url('../assets/jb.jpg') no-repeat center center;
+                }
+            }
+        }
+        .right{
+            width: 30%;
+            button{
+                height: 100%;
+                float: right;
+                background: #fff;
+                padding: 0 .2rem;
+                border: .02rem solid #ccc;
+                border-radius: .1rem;
+                color: #ccc;
+            }
+        }
+    }
+    .banner{
+        height: 1.1rem;
+        width: 100%;
+        img{
+            width: 100%;
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Top />
+    <footer>
+      <div>
+        <img src="../assets/scroll_02.png" alt="" />
+        <span>电话咨询</span>
+      </div>
+      <div>
+        <img src="../assets/scroll_01.png" alt="" />
+        <span>在线咨询</span>
+      </div>
+      <div>
+        <img src="../assets/scroll_03.png" alt="" />
+        <span>网上报名</span>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Top from "../components/top.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Top,
+  },
+};
+</script>
+<style lang="less">
+.home {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  float: left;
+  background: #eee;
+  overflow: hidden;
+  border-radius: 0.04rem;
+  footer {
+    display: flex;
+    justify-content: space-around;
+    font-size: 0.15rem;
+    font-weight: 700;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    div {
+      width: 33%;
+      background: #2b4d8b;
+      text-align: center;
+      color: white;
+      padding: 0.1rem 0;
+      // line-height: .4rem;
+      display: flex;
+      justify-content: space-around;
+      align-content: center;
+        span{
+          
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        }
+    }
   }
 }
-</script>
+</style>
